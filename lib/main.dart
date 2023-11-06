41a110d6ad82697f6ba7f644fb16626afaea0990
Abu Salek <64322352+salek877@users.noreply.github.com>
2023-11-06 14:48:55 +0600

    ui updated
    Integrated API

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,24 @@ import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
 void main() {
-  runApp(CoffeeLandClassifierApp());
+  runApp(const CoffeeLandClassifierApp());
 }
 
 class CoffeeLandClassifierApp extends StatelessWidget {
+  const CoffeeLandClassifierApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
 }
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -37,44 +42,50 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  Future<void> _predictImage() async {
-    if (_imageFile == null) return;
+ Future<void> _predictImage() async {
+  if (_imageFile == null) return;
 
-    final uri = Uri.parse('API endpoint'); //API endpoint
+  final uri = Uri.parse('http://localhost:8000/predict/'); //API endpoint
+  final request = http.MultipartRequest('POST', uri);
 
-    final request = http.MultipartRequest('POST', uri);
-    request.files.add(
-      await http.MultipartFile.fromPath(
-        'file',
-        _imageFile!.path,
-      ),
-    );
+  request.files.add(
+    await http.MultipartFile.fromPath(
+      'upload_file',
+      _imageFile!.path,
+    ),
+  );
 
-    try {
-      final response = await request.send();
-      if (response.statusCode == 200) {
-        final responseBody = await response.stream.bytesToString();
-        final result = jsonDecode(responseBody);
-        setState(() {
-          _predictionResult = result['class'];
-        });
-      } else {
-        setState(() {
-          _predictionResult = 'Error';
-        });
-      }
-    } catch (e) {
+  try {
+    final response = await request.send();
+    final responseBody = await response.stream.bytesToString();
+
+    if (response.statusCode == 200) {
+      final result = jsonDecode(responseBody);
+      print('res ${result}');
+      final predictionResult = '${result}'; 
       setState(() {
-        _predictionResult = 'Error: $e';
+        _predictionResult = predictionResult;
+      });
+    } else {
+      print('Server responded with status code: ${response.statusCode}');
+      print('Response body: $responseBody');
+      setState(() {
+        _predictionResult = 'Error: Server responded with status code: ${response.statusCode}';
       });
     }
+  } catch (e) {
+    print('Error: $e');
+    setState(() {
+      _predictionResult = 'Error: $e';
+    });
   }
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Coffee Land Classifier'),
+        title: const Text('Coffee Land Classifier'),
       ),
       body: Center(
         child: Column(
@@ -87,17 +98,18 @@ class _HomeScreenState extends State<HomeScreen> {
                 height: 200,
               )
             else
-              Text('No image selected'),
-            SizedBox(height: 20),
+              const Text('No image selected'),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _selectImage,
-              child: Text('Select Image'),
+              child: const Text('Select Image'),
             ),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _predictImage,
-              child: Text('Predict'),
+              child: const Text('Predict'),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Text('Prediction Result: $_predictionResult'),
           ],
         ),
